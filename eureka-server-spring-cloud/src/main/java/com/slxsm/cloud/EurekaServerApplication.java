ce3b01817518d2f6e931cde6d3d7293658e8e5b6
18565855370 <46896949+18565855370@users.noreply.github.com>
2019-04-20 22:51:25 +0800

    提交eureka
    第一次提交eureka

---
+++ eureka-server-spring-cloud/src/main/java/com/slxsm/cloud/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.slxsm.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello Eureka
+ * @author slxsm
+ * @date 2019-04-20
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
